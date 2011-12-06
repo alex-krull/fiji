2416d204f6b391e78fbaaa86e8b022844f6905b6
krull@mpi-cbg.de <alex@alex-desktop.(none)>
2011-12-06 17:29:30 +0100

    refactoring

--- src-plugins/Cell_Blob_Tracking/blobTracking/BlobGui.java
+++ src-plugins/Cell_Blob_Tracking/blobTracking/BlobGui.java
@@ -1,8 +1,18 @@
 package blobTracking;
 
+import ij.ImagePlus;
+
+import java.awt.event.MouseEvent;
+
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.Type;
 import frameWork.Gui;
 
-public class BlobGui <IT extends Type<IT> > extends Gui<IT>{
+public class BlobGui <IT extends Type<IT> > extends Gui<IT> {
+	
+public BlobGui(RandomAccessibleInterval<IT> img){
+	image=img;
+}
 
+	
 }
--- src-plugins/Cell_Blob_Tracking/frameWork/Gui.java
+++ src-plugins/Cell_Blob_Tracking/frameWork/Gui.java
@@ -1,9 +1,50 @@
 package frameWork;
 
+import ij.ImageListener;
+import ij.ImagePlus;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
 import net.imglib2.type.Type;
 
-public abstract class Gui<IT extends Type<IT>> {
-protected Img <IT> image;
+public abstract class Gui<IT extends Type<IT>> implements MouseMotionListener, ImageListener {
+
+	protected RandomAccessibleInterval<IT> image;
+
+	
+	
+
+@Override
+public void mouseDragged(MouseEvent arg0) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseMoved(MouseEvent arg0) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void imageClosed(ImagePlus arg0) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void imageOpened(ImagePlus arg0) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void imageUpdated(ImagePlus arg0) {
+	// TODO Auto-generated method stub
+	
+}
 
 }
