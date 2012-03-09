552d42a7568bdac0f46ff1ca3228c2c1bc825bf5
Ramunno <ramunno@tolic-mac-21.mpi-cbg.de>
2012-03-09 14:26:04 +0100

    Damien's GUI is Added

---
+++ src-plugins/Cell_Blob_Tracking/frameWork/gui/ControlWindow/ControlWindow2.java
@@ -0,0 +1,352 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import javax.swing.table.*;
+import javax.swing.JTextField;
+
+
+
+
+public class ControlWindow2  {
+
+	/**
+	 * @param args
+	 */
+
+
+	JTextField currentSession;
+	JTextField currentMethod;
+	ArrayList<Object> sessionList = new ArrayList();
+	JFrame frame;
+	JPanel rightPanel;
+	JList visList;
+
+	public static void main(String[] args) {
+
+		ControlWindow2 window = new ControlWindow2();
+
+		window.go();
+
+
+	}
+
+	public void go(){
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+		rightPanel = new JPanel();
+		JPanel leftPanel = new JPanel();
+		JPanel centerPanel = new JPanel();
+		JPanel bottomPanel = new JPanel();
+		// Some constants
+		Dimension labelDim = new Dimension(60, 28);
+		Dimension labelDim2 = new Dimension(60, 19);
+
+
+		//Bottom Area
+		bottomPanel.setBackground(Color.LIGHT_GRAY);
+
+		JTextArea text = new JTextArea(5,5);
+
+		text.setLineWrap(true);
+
+		JScrollPane scroller = new JScrollPane(text);
+		scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+
+		//bottomPanel.add(scroller);
+
+		//This controls the left Panel
+		JButton merge = new JButton("Merge");
+		JButton delete = new JButton("Delete");
+		JButton jump = new JButton("Go to  ");
+		JButton split = new JButton("Spilt");
+		JButton trim = new JButton("Trim");
+
+
+
+		leftPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+
+
+		leftPanel.add(merge);
+		leftPanel.add(split);
+		leftPanel.add(trim);
+		leftPanel.add(delete);
+		leftPanel.add(jump);
+
+
+		// The controls on the right
+
+		rightPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+
+		SpinnerModel model1 = new SpinnerNumberModel();
+		JSpinner spinner1 = new JSpinner(model1);
+
+		JLabel label1 = new JLabel("Stack #");
+		label1.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+
+
+		rightPanel.add(label1);
+		rightPanel.add(spinner1);
+
+		spinner1.setPreferredSize(labelDim2);
+		spinner1.setMaximumSize(labelDim);
+
+		SpinnerModel model2 = new SpinnerNumberModel();
+		JSpinner spinner2 = new JSpinner(model2);
+		spinner2.setPreferredSize(labelDim2);
+		spinner2.setMaximumSize(labelDim);
+		JLabel label2 = new JLabel("Z #");
+		label2.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		rightPanel.add(label2);
+		rightPanel.add(spinner2);
+
+		SpinnerModel model3 = new SpinnerNumberModel(20, 0, 40, 1);
+		JSpinner spinner3 = new JSpinner(model3);
+		spinner3.setPreferredSize(labelDim2);
+		spinner3.setMaximumSize(labelDim);
+		JLabel label3 = new JLabel("Channel #");
+		label3.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		rightPanel.add(label3);
+		rightPanel.add(spinner3);
+
+		//Create the tracking method pair.
+
+		JLabel label4 = new JLabel("Current Method");
+		label4.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		rightPanel.add(label4);
+
+		String currentM = "";
+
+		currentMethod = new JTextField();
+		currentMethod.setText(currentM);
+		currentMethod.setHorizontalAlignment(JTextField.CENTER);
+		currentMethod.setMaximumSize(new Dimension(1000, 30));
+		currentMethod.setEditable(false);
+
+
+		rightPanel.add(currentMethod);
+
+		JLabel labelSession = new JLabel("Current Session");
+		labelSession.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		rightPanel.add(labelSession);
+
+		String currentS = "";
+
+		currentSession = new JTextField();
+		currentSession.setText(currentS);
+		currentSession.setHorizontalAlignment(JTextField.CENTER);
+		currentSession.setMaximumSize(new Dimension(1000, 30));
+		currentSession.setEditable(false);
+
+
+		rightPanel.add(currentSession);
+
+		/*JLabel labelWhich = new JLabel("Pick Session ");
+		labelWhich.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		rightPanel.add(labelWhich);
+
+		String[] whichSession = {"Session1", "Session2", "Session3"}; //get month names
+        SpinnerListModel trackMethodsModel = null;
+
+        SpinnerListModel whichSessionModel = new SpinnerListModel(whichSession);
+
+        JSpinner sessionSpinner = new JSpinner(whichSessionModel);
+        sessionSpinner.setPreferredSize(new Dimension(90, 25));
+        sessionSpinner.setMaximumSize(new Dimension(90, 25));
+
+
+
+        sessionSpinner.setAlignmentX(JSpinner.CENTER_ALIGNMENT);
+
+        JFormattedTextField ftf = getTextField(sessionSpinner);
+        ftf.setHorizontalAlignment(JTextField.CENTER);
+
+
+        rightPanel.add(sessionSpinner);*/
+
+
+
+		JLabel label5 = new JLabel("Visable Sessions");
+		label5.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		rightPanel.add(label5);
+
+		String[] vis = {"No Sessions"};
+		
+
+		
+		visList = new JList(vis);
+		//visList.setVisibleRowCount(3);
+		//visList.setBorder(BorderFactory.createLineBorder(Color.black));
+		visList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+
+		JScrollPane visScroller = new JScrollPane(visList);
+		visScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		visScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		visScroller.setMaximumSize(new Dimension(1000, 100));
+		
+		rightPanel.add(visScroller);
+		
+		JButton start = new JButton("Start Tracking");
+		start.setAlignmentX(JButton.CENTER_ALIGNMENT);
+		rightPanel.add(start);
+
+		JButton newSession = new JButton("New Session");
+		newSession.addActionListener(new NewSessionListener());
+		newSession.setAlignmentX(JButton.CENTER_ALIGNMENT);
+		rightPanel.add(newSession);
+
+		JButton deleteSession = new JButton("Delete Session");
+		//deleteSession.addActionListener(new NewSessionListener());
+		deleteSession.setAlignmentX(JButton.CENTER_ALIGNMENT);
+		rightPanel.add(deleteSession);
+
+		JButton changeSession = new JButton("Change Session");
+		//deleteSession.addActionListener(new NewSessionListener());
+		changeSession.setAlignmentX(JButton.CENTER_ALIGNMENT);
+		changeSession.addActionListener(new ChangeSessionListener());
+		rightPanel.add(changeSession);
+
+		//This controls the center panel
+
+		centerPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+
+
+
+
+		TableSort newContentPane = new TableSort();
+		newContentPane.setOpaque(true);
+
+
+
+		centerPanel.add(newContentPane);
+
+		JTextArea details = new JTextArea("Place holder");
+		Border raisedetched = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
+		details.setBorder(raisedetched);
+		centerPanel.add(details);
+
+		// The is the menu bar area
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+
+		JMenuItem newMenuItem = new JMenuItem("New");
+		fileMenu.add(newMenuItem);
+		menuBar.add(fileMenu);
+		menuBar.setBackground(Color.white);
+		menuBar.setForeground(Color.BLACK);
+
+		frame.setJMenuBar(menuBar);
+
+		// This makes the Dialog for tracking method
+
+
+		// This puts everything in the frame
+		frame.getContentPane().add(BorderLayout.SOUTH, scroller);
+		frame.getContentPane().add(BorderLayout.EAST, rightPanel);
+		frame.getContentPane().add(BorderLayout.CENTER, centerPanel);
+		frame.getContentPane().add(BorderLayout.WEST, leftPanel);
+		frame.setSize(1000,600);
+		frame.setVisible(true);
+
+
+
+	}
+
+
+
+
+
+	public JFormattedTextField getTextField(JSpinner spinner) {
+		JComponent editor = spinner.getEditor();
+		if (editor instanceof JSpinner.DefaultEditor) {
+			return ((JSpinner.DefaultEditor)editor).getTextField();
+		} else {
+			System.err.println("Unexpected editor type: "
+					+ spinner.getEditor().getClass()
+					+ " isn't a descendant of DefaultEditor");
+			return null;
+		}
+	}
+
+	//This creates the newSessionDialog
+	public void newSessionDialog() {
+
+		JDialog.setDefaultLookAndFeelDecorated(true);
+
+
+
+		
+		Object selection = JOptionPane.showInputDialog(null, "Enter new session name:","Create new session",
+				 JOptionPane.PLAIN_MESSAGE);
+		currentMethod.setText((String) selection);
+		sessionList.add(selection);
+
+		String[] newValues = new String[sessionList.size()];
+		sessionList.toArray(newValues);
+		
+		visList.setListData(newValues);
+		/*JButton test = new JButton("test");
+		rightPanel.add(test);
+		frame.validate();
+		frame.repaint();*/
+		//Object[] possibilities = {"ham", "spam", "yam"};
+		//String s = (String)JOptionPane.showInputDialog(frame);
+
+	} 
+
+	public void changeSessionDialog() {
+
+		JDialog.setDefaultLookAndFeelDecorated(true);
+
+
+		//Object[] selectionValues = { "Session 1", "Session 2", "Session 3" };
+		
+		Object[] selectionValues = new Object[sessionList.size()];
+		sessionList.toArray(selectionValues);
+		String initialSelection = "Cell Tracking";
+		Object selection = JOptionPane.showInputDialog(null, "Pick session to change to",
+				"Tracking Method", JOptionPane.PLAIN_MESSAGE, null, selectionValues, initialSelection);
+		currentSession.setText((String) selection);
+
+
+		//Object[] possibilities = {"ham", "spam", "yam"};
+		//String s = (String)JOptionPane.showInputDialog(frame);
+
+	} 
+
+
+
+	public class ChangeSessionListener implements ActionListener {
+		public void actionPerformed(ActionEvent s) {
+			changeSessionDialog();
+
+
+
+		}
+
+	}
+
+
+	public class NewSessionListener implements ActionListener {
+		public void actionPerformed(ActionEvent a) {
+			newSessionDialog();
+
+
+
+		}
+
+	}
+}
---
+++ src-plugins/Cell_Blob_Tracking/frameWork/gui/ControlWindow/NewSessionDialog.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+import javax.swing.JDialog;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.BoxLayout;
+import javax.swing.Box;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.beans.*; //Property change stuff
+import java.awt.*;
+import java.awt.event.*;
+
+public class NewSessionDialog {
+
+	Object[] possibilities = {"ham", "spam", "yam"};
+	String s = (String)JOptionPane.showInputDialog(possibilities);
+	
+}
