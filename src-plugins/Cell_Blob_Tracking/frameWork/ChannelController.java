ce2e8ca84f82bf2f311a164eb3ffd550a6dca102
Alexander Krull <krull@mpi-cbg.de>
2012-04-26 20:03:13 +0200

    no manipulation during tracking

--- src-plugins/Cell_Blob_Tracking/frameWork/ChannelController.java
+++ src-plugins/Cell_Blob_Tracking/frameWork/ChannelController.java
@@ -31,7 +31,7 @@ public class ChannelController<T extends Trackable,  IT extends  NumericType<IT>
 	
 	public void click(long[] pos, MouseEvent e, ViewModel<IT> vm){
 		try{
-		
+		if (model.isCurrentlyTracking()) return;
 		selectedSequenceId= policy.click(pos, e, model, selectedIdList, trackingChannel, selectedSequenceId, vm);
 		}catch(Exception ex){
 			ex.printStackTrace(Model.errorWriter);
