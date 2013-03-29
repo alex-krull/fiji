3887a5fe9151d9b3051557369105fc8cb90e310c
Damien <ramunnoj@chhe004.ohsu.edu>
2013-03-29 15:04:34 -0700

    Added GPL License to all files, needs to be updated when paper is accepted somewhere

--- src-plugins/Cell_Blob_Tracking/src/main/java/frameWork/gui/KymoWindow.java
+++ src-plugins/Cell_Blob_Tracking/src/main/java/frameWork/gui/KymoWindow.java
@@ -1,3 +1,27 @@
+/*******************************************************************************
+ * This software implements the tracking method descibed in the following paper: 
+ * "A divide and conquer strategy for the maximum likelihood localization of ultra low intensity objects"
+ *  By Alexander Krull et Al, 2013. (Enter final journal)
+ *
+ * Copyright (c) 2013 Alexandar Krull
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * Contributors:
+ * 	Alexander Krull (Alexander.Krull@tu-dresden.de)
+ *     Damien Ramunno-Johnson (GUI)
+ *******************************************************************************/
 package frameWork.gui;
 import java.awt.Scrollbar;
 import java.awt.event.AdjustmentEvent;
